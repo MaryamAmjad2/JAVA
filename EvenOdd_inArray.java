78132792ae4911b331615d8f7c5903b8bc92196e
Maryam Amjad <87093456+MaryamAmjad2@users.noreply.github.com>
2022-11-04 17:18:07 +0500

    Add files via upload

---
+++ EvenOdd_inArray.java
@@ -0,0 +1,20 @@
+
+package javaapplication15;
+
+
+public class JavaApplication15 {
+
+    
+    public static void main(String[] args) {
+      for(int num=1;num<=10;num++)
+      {
+          if(num% 2==0);
+          {
+          System.out.println("number "+num +" is even");}
+          else{
+         System.out.print("odd");}
+      }
+    }
+    
+    
+}
